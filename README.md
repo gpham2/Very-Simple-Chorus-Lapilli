2b3fed87a073fa721ef048b28f61aa67806a7173
gpham2 <giangphamgia03@gmail.com>
2022-05-11 01:43:34 -0700

    local to github repo

--- README.md
+++ README.md
@@ -68,3 +68,151 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+Implentation:
+
+1. Implement a count for turn
+	this.state = {
+        turn: 0,
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      };
+
+2. Made it so you can't place any new 'X' or 'O' after turn 6
+	if (this.state.turn > 5 && squares[i] == null) {
+          return;
+      }
+
+3. Made it so you can chose which existing X's or O's to move
+	if (this.state.selectedNext === false && this.openAdj(i)) {
+            squares[i] = "'" + squares[i] + "'";
+            this.setState({
+                prevI: i,
+                selectedNext: true,
+                squares: squares,
+            });       
+          }
+
+4. Made it so you can change the selection if it is not moved yet
+	else if (this.state.selectedNext === true && this.openAdj(i) &&
+               i !== this.state.prevI) {
+            const mark = Array.from(squares[this.state.prevI]);
+            squares[i] = "'" + squares[i] + "'";
+            squares[this.state.prevI] = mark[1];
+            this.setState({
+                prevI: i,
+                selectedNext: true,
+                squares: squares,
+    
+            });
+          }
+
+5. Made a function that checks for adjacent openings
+	openAdj(i) {
+        const squares = this.state.squares;
+        if (i === 0 && (!squares[1] || !squares[3] || !squares[4])) return true;
+        if (i === 1 && ( squares[6] ||  squares[7] ||  squares[8])) return true;
+        if (i === 2 && (!squares[1] || !squares[4] || !squares[5])) return true;
+        if (i === 3 && ( squares[2] ||  squares[5] ||  squares[8])) return true;
+        if (i === 4) return true;
+        if (i === 5 && ( squares[0] ||  squares[3] ||  squares[6])) return true;
+        if (i === 6 && (!squares[3] || !squares[4] || !squares[7])) return true;
+        if (i === 7 && ( squares[0] ||  squares[1] ||  squares[2])) return true;
+        if (i === 8 && (!squares[4] || !squares[5] || !squares[7])) return true;
+        return false;        
+    }
+
+6. Made a function that checks for valid adjacent destinations
+	validDest(i, j) {
+        if ( Math.abs(i - j) === 3 || ( Math.abs(i - j) === 1 &&
+	     (i + j !== 7 && i + j !== 13))) return true;
+        if ( Math.abs(i - j) === 4 || ( Math.abs(i - j) === 2 &&
+           (i + j !== 4 && i + j !== 10 && i + j !== 16))) return true;
+        return false;
+    }
+
+7. Implement the "moving" of the X's or O's
+	const mark = Array.from(squares[this.state.prevI]);
+        let flag = true;
+        if ((squares[4] === 'X' && this.state.xIsNext === true) ||
+             (squares[4] === 'O' && this.state.xIsNext === false))
+            flag = false;
+        squares[i] = mark[1];
+        squares[this.state.prevI] = null;
+        if (this.validDest(i + 1, this.state.prevI + 1) && 
+           (calculateWinner(squares) || (flag))) {
+            this.setState({
+                prevI: -1,
+                selectedNext: false,
+                squares: squares,
+                xIsNext: !this.state.xIsNext,
+            });
+        }
+
+8. Making sure that during X turn, only X's can be selected
+	if ((this.state.xIsNext === true && squares[i] === 'O') ||
+          (this.state.xIsNext === false && squares[i] === 'X'))
+            return;
+
+9. Done
+
+
+HOW TO PLAY:
+
+1. cd into chorus-lappilli and npm start to launch the game
+
+2. X goes first, simply click one of the 9 squares to place your position
+
+3. Then it's O's turn. O selects an open square. This repeats for 6 turns.
+
+4. After 3 X's and 3 O's are on the board, no more marks can be placed.
+
+5. To continue, the next player must click one of his or her marks.
+
+6. The selected mark will be indicated with a 'X' or 'O'.
+
+7. Only marks with existing adjacent empty tiles can be selected
+
+8. If the mark is selected but have not been moved, but player is free
+   to chose another legal mark.
+
+9. After this, the player can chose to move the mark into an adjacent
+   empty tile.
+
+10. If one of the player's marks is in the center square, the player
+    must either win or vacate the center square with their move
+
+11. This cycles among the two players until there is a winner.
+
+
+
+npm pack --dry-run:
+
+npm notice 
+npm notice 📦  chorus-lappilli@0.1.0
+npm notice === Tarball Contents ===
+npm notice 3.4kB  README.md
+npm notice 23.6kB chorus-lappilli-0.1.0.tgz
+npm notice 818B   package.json
+npm notice 3.9kB  public/favicon.ico
+npm notice 1.7kB  public/index.html
+npm notice 5.3kB  public/logo192.png
+npm notice 9.7kB  public/logo512.png
+npm notice 492B   public/manifest.json
+npm notice 67B    public/robots.txt
+npm notice 789B   src/index.css
+npm notice 5.7kB  src/index.js
+npm notice === Tarball Details ===
+npm notice name:          chorus-lappilli
+npm notice version:       0.1.0
+npm notice filename:      chorus-lappilli-0.1.0.tgz
+npm notice package size:  47.3 kB
+npm notice unpacked size: 55.4 kB
+npm notice shasum:        d7fad8b237033aa2bbaf9c73d88751744c819d27
+npm notice integrity:     sha512-6ljdpM7I9n6gM[...]ToPr4q8Pd9Sog==
+npm notice total files:   11
+npm notice
+	
+	
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,188 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  
+  class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        turn: 0,
+        squares: Array(9).fill(null),
+        xIsNext: true,
+        selectedNext: false,
+        prevI: -1,
+      };
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  
+    openAdj(i) {
+        const squares = this.state.squares;
+        if (i === 0 && (!squares[1] || !squares[3] || !squares[4])) return true;
+        if (i === 1 && ( squares[6] ||  squares[7] ||  squares[8])) return true;
+        if (i === 2 && (!squares[1] || !squares[4] || !squares[5])) return true;
+        if (i === 3 && ( squares[2] ||  squares[5] ||  squares[8])) return true;
+        if (i === 4) return true;
+        if (i === 5 && ( squares[0] ||  squares[3] ||  squares[6])) return true;
+        if (i === 6 && (!squares[3] || !squares[4] || !squares[7])) return true;
+        if (i === 7 && ( squares[0] ||  squares[1] ||  squares[2])) return true;
+        if (i === 8 && (!squares[4] || !squares[5] || !squares[7])) return true;
+        return false;        
+    }
+
+    validDest(i, j) {
+        if ( Math.abs(i - j) === 3 || ( Math.abs(i - j) === 1 && (i + j !== 7 && i + j !== 13))) return true;
+        if ( Math.abs(i - j) === 4 || ( Math.abs(i - j) === 2 && (i + j !== 4 && i + j !== 10 && i + j !== 16))) return true;
+        return false;
+
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      if (calculateWinner(squares)) return;
+      if (this.state.turn > 5 && squares[i] == null && this.state.selectedNext === false) return;
+      if (this.state.turn < 6 && (calculateWinner(squares) || squares[i])) return;
+
+
+      if (this.state.turn < 6) {
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            turn: this.state.turn + 1,
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+      }
+
+      else if (squares[i] !== null) {
+          if ((this.state.xIsNext === true && squares[i] === 'O') || (this.state.xIsNext === false && squares[i] === 'X'))
+            return;
+          if (this.state.selectedNext === false && this.openAdj(i)) {
+            squares[i] = "'" + squares[i] + "'";
+            this.setState({
+                prevI: i,
+                selectedNext: true,
+                squares: squares,
+    
+            });       
+          }
+          else if (this.state.selectedNext === true && this.openAdj(i) && i !== this.state.prevI) {
+            const mark = Array.from(squares[this.state.prevI]);
+            squares[i] = "'" + squares[i] + "'";
+            squares[this.state.prevI] = mark[1];
+            this.setState({
+                prevI: i,
+                selectedNext: true,
+                squares: squares,
+    
+            });
+          }
+      }
+      else {
+        const mark = Array.from(squares[this.state.prevI]);
+        let flag = true;
+        if ((squares[4] === 'X' && this.state.xIsNext === true) || (squares[4] === 'O' && this.state.xIsNext === false))
+            flag = false;
+        squares[i] = mark[1];
+        squares[this.state.prevI] = null;
+        if (this.validDest(i + 1, this.state.prevI + 1) && (calculateWinner(squares) || (flag))) {
+            this.setState({
+                prevI: -1,
+                selectedNext: false,
+                squares: squares,
+                xIsNext: !this.state.xIsNext,
+            });
+        }
+        
+      }
+      
+    }
+  
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+  
+    render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+          </div>
+        </div>
+      );
+    }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);
+  
+  
